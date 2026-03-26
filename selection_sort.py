4193d5188e5be8b79c85d3ff52c0bf1cebeb8b80
Michalmaciej <mich.kowa.01@gmail.com>
2026-03-26 14:24:18 +0100

    Selection sort algorithm

--- selection_sort.py
+++ selection_sort.py
@@ -1,5 +1,16 @@
 import random
 
+#creating unsorted list with the numbers
 numbers = random.sample(range(1, 1001), 500)
 
-print(numbers[0:50])
+for i in range(len(numbers)-1):
+    smallest = 1001
+    index = 0
+    for j in range(i+1, len(numbers)):
+        if numbers[j] < smallest:
+            smallest = numbers[j]
+            index = j
+    if numbers[i] > smallest:
+        numbers[i], numbers[index] = smallest, numbers[i]
+
+#print(numbers[400:500])
